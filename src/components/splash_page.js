8974356daec9efb653e9e33c0cb849885aee3b3c
Andre Nguyen <ahnguye@ucdavis.edu>
2018-05-17 19:19:12 -0700

    layout

--- src/components/splash_page.js
+++ src/components/splash_page.js
@@ -7,6 +7,40 @@ export default class SplashPage extends Component {
       <div>
         <img className="banner" src="https://d1raprm0ezfai1.cloudfront.net/Website+images+used/Photo+Fillers/_shutterstock_658353277.jpg" />
 
+        Designer nails made for you, delivered to your door
+        <div className="row icons">
+          <div className="col-12 col-md-2 icon-container">
+            <img className="icon" src="https://d1raprm0ezfai1.cloudfront.net/Website+images+used/Photo+Fillers/manicure.png" />
+            Tailor-fitted, ready-to-wear nails made with cutting edge 3D printing and scanning technologies.
+          </div>
+          <div className="col-12 col-md-2 icon-container">
+            <img className="icon" src="https://d1raprm0ezfai1.cloudfront.net/Website+images+used/Photo+Fillers/safety.png" />
+            Nail-safe, easy-to-remove adhesives allow you to switch your style as often as you'd like.
+          </div>
+          <div className="col-12 col-md-2 icon-container">
+            <img className="icon" src="https://d1raprm0ezfai1.cloudfront.net/Website+images+used/Photo+Fillers/view.png" />
+            Ever growing design gallery featuring acclaimed international artists to inspire your style.
+          </div>
+          <div className="col-12 col-md-2 icon-container">
+            <img className="icon" src="https://d1raprm0ezfai1.cloudfront.net/Website+images+used/Photo+Fillers/nail.png" />
+            Each set is delivered to your door so you can sit back, relax, and be perfectly polished.</div>
+        </div>
+
+        Three ways to be perfectly polished
+
+        <div className="row icons">
+          <div className="col-12 col-md-3 icon-container">
+            <img className="icon" src="https://d1raprm0ezfai1.cloudfront.net/Website+images+used/Photo+Fillers/shutterstock_514409545.jpg" />
+          </div>
+          <div className="col-12 col-md-3 icon-container">
+            <img className="icon" src="https://d1raprm0ezfai1.cloudfront.net/Website+images+used/Photo+Fillers/brigitte-tohm-388989.jpg" />
+          </div>
+          <div className="col-12 col-md-3 icon-container">
+            <img className="icon" src="https://d1raprm0ezfai1.cloudfront.net/Website+images+used/Photo+Fillers/_shutterstock_735672685.jpg" />
+          </div>
+        </div>
+
+
       </div>
     );
   }
