916f7a311db0093817dc6561b35422d7778ba550
Andre Nguyen <ahnguye@ucdavis.edu>
2018-05-19 18:41:49 -0700

    charge token

--- src/components/cart_container_component.js
+++ src/components/cart_container_component.js
@@ -20,7 +20,7 @@ class CartContainerComponent extends React.Component {
 
   onToken = (token, amount, description) => {
     console.log("called");
-    axios.post("localhost:8080",
+    axios.post("http://880bc039.ngrok.io/charge",
       {
         description,
         source: token.id,
