0541cadddd32da6085649da3b2303e926c3e7780
Andre Nguyen <ahnguye@ucdavis.edu>
2018-05-21 07:47:32 -0700

    froward

--- server.js
+++ server.js
@@ -5,7 +5,15 @@ const app = express();
 
 app.use(express.static(__dirname));
 
+app.use((req, res, next) => {
+  if (req.header('x-forwarded-proto') !== 'https')
+    res.redirect(`https://${req.header('host')}${req.url}`)
+  else
+    next()
+})
+
 app.get('*', (req,res) => {
+  console.log(__dirname);
   res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
