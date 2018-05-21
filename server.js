cdb9b2e0cf2bc7236fda9bbda6c515a2d44be662
Andre Nguyen <ahnguye@ucdavis.edu>
2018-05-21 07:30:34 -0700

    https

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
+const enforce = require('express-sslify');
 
 app.use(express.static(__dirname));
-
+app.use(enforce.HTTPS());
 app.get('*', (req,res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'));
 });
