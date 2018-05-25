d432e47468c76d3aae7281ac6f076546f647da6a
Andre Nguyen <ahnguye@ucdavis.edu>
2018-05-25 13:46:55 -0700

    tech component

--- src/components/about_component.js
+++ src/components/about_component.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+//import '../../style/about.css';
 
 class AboutComponent extends Component {
   constructor(props){
@@ -7,8 +8,30 @@ class AboutComponent extends Component {
 
   render() {
     return (
-      <div className="">
-        <h2>About</h2>
+      <div className="row about-profile-height">
+        <div className="col-12 col-md-3 no-margin no-overflow">
+          <div className="about-profile-img-container">
+            <img className="about-profile-img" src="https://d1raprm0ezfai1.cloudfront.net/Website+images+used/26239593_1256384884462568_199943898728075355_n.jpg" />
+          </div>
+
+        </div>
+        <div className="col-12 col-md-3 about-profile-img-text-container text-align-center bg-teal">
+          <span className="about-profile-title"><span className="bold">OUR</span></span>
+          <br/>
+          <span className="about-profile-title">TEAM</span>
+          <br/><span className="big-backslash belinda">/</span>
+          <br/><div className="about-profile-description">Our team is excited to solve this problem for ourselves and the awesome women in our lives. We hail from Taiwan, Korea, and Spain. Our strength lies in our team's experiences as strategy consultants, nail technicians, 3D printing manufacturing analysts, and entrepreneurs.</div>
+        </div>
+        <div className="col-12 col-md-3 no-margin no-overflow">
+          <div className="about-profile-img-container">
+            <img className="about-profile-img" src="https://d1raprm0ezfai1.cloudfront.net/Website+images+used/26239593_1256384884462568_199943898728075355_n.jpg" />
+          </div>
+        </div>
+        <div className="col-12 col-md-3 no-margin no-overflow">
+          <div className="about-profile-img-container">
+            <img className="about-profile-img" src="https://d1raprm0ezfai1.cloudfront.net/Website+images+used/26239593_1256384884462568_199943898728075355_n.jpg" />
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/our_technology_component.js
+++ src/components/our_technology_component.js
@@ -7,9 +7,44 @@ class OurTechnologyComponent extends Component {
 
   render() {
     return (
-      <div className="">
-        <h2>Our Technology</h2>
+      <div>
+        <div className="row about-tech-container no-overflow">
+          <div className="col-12 col-md-5 about-tech-img-text-container bg-black font-white">
+            <span className="about-profile-title"><span className="bold">OUR</span></span>
+            <br/>
+            <span className="about-profile-title">TECHNOLOGY</span>
+            <br/><span className="big-backslash belinda">/</span>
+            <br/><br/><div className="">We're bringing technology to our art so you can be perfectly polished, hassle-free</div>
+          </div>
+
+          <div className="col-12 col-md-7 no-margin no-overflow">
+            <div className="about-profile-img-container">
+              <img className="about-technology-img" src="https://d1raprm0ezfai1.cloudfront.net/Website+images+used/26239593_1256384884462568_199943898728075355_n.jpg" />
+            </div>
+          </div>
+        </div>
+        <div className="row text-align-center">
+          <div className="col-12 col-md-3 no-margin no-overflow bg-grey about-tech-item-container">
+            <div className="tech-item-title avenir font-white">PHOTOGRAMMETRY</div>
+            <br /><div className="tech-item-description font-white">Our photogrammetry algorithm helps us measure your nails to get your tailor-fitted</div>
+          </div>
+          <div className="col-12 col-md-3 no-margin no-overflow bg-yellow about-item-tech-container">
+            <div className="tech-item-title avenir font-white">3D PRINTING</div>
+            <br /><div className="tech-item-description font-white">We use additive manufacturing to custom print your perfect fit nails</div>
+          </div>
+          <div className="col-12 col-md-3 no-margin no-overflow bg-purple about-item-tech-container">
+            <div className="tech-item-title avenir font-white">SEMI-CURED GELS</div>
+            <br /><div className="tech-item-description font-white">Our semi-cured technology ensures the nails are flexible and durable</div>
+
+          </div>
+          <div className="col-12 col-md-3 no-margin no-overflow bg-tan about-item-tech-container">
+            <div className="tech-item-title avenir font-white">MACHINE LEARNING</div>
+            <br /><div className="tech-item-description font-white">We use machine learning to learn your style so we're always showing you new trends that inspire you</div>
+
+          </div>
+        </div>
       </div>
+
     );
   }
 }
