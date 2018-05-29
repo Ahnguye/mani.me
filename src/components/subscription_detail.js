40e57511ad041957f82824c4d16b8468348043d9
Andre Nguyen <ahnguye@ucdavis.edu>
2018-05-29 14:44:03 -0700

    subscription queries

--- src/components/subscription_detail.js
+++ src/components/subscription_detail.js
@@ -10,38 +10,38 @@ class SubscriptionDetailComponent extends Component {
     return (
       <div className="">
         <div className="subscription-banner-wrapper">
-          <img className="subscription-banner-picture" src="https://d1raprm0ezfai1.cloudfront.net/Website+images+used/subscriptions/subscription1.jpg" />
+          <img className="subscription-banner-picture" src="" />
           <div className="subscription-banner-title font-white avenir-book">TREAT.ME</div>
         </div>
         <div className="icons-title avenir"><h1 className="">How It Works</h1></div>
         <div className="subscription-gold-line"></div>
         <div className="icons no-margin-vertical">
-          <div className="row justify-content-center">
-            <div className="col-12 col-md-2 icon-container">
+          <div className="row justify-content-center subscription-icon-container">
+            <div className="col-12 col-md-3 icon-container">
               <img className="icon" src="https://d1raprm0ezfai1.cloudfront.net/Website+images+used/Photo+Fillers/view.png" />
               <div className="icon-line"></div>
-              <div className="icon-text">
+              <div className="subscription-icon-text">
                 Pick a design you love from our artist gallery for your special treat
               </div>
             </div>
-            <div className="col-12 col-md-2 icon-container">
+            <div className="col-12 col-md-3 icon-container">
               <img className="icon" src="https://d1raprm0ezfai1.cloudfront.net/Website+images+used/Photo+Fillers/safety.png" />
               <div className="icon-line"></div>
-              <div className="icon-text">
+              <div className="subscription-icon-text">
                 Customize the length, shape, and colors to fit your style
               </div>
             </div>
-            <div className="col-12 col-md-2 icon-container">
+            <div className="col-12 col-md-3 icon-container">
               <img className="icon" src="https://d1raprm0ezfai1.cloudfront.net/Website+images+used/Photo+Fillers/manicure.png" />
               <div className="icon-line"></div>
-              <div className="icon-text">
+              <div className="subscription-icon-text">
                 Send us 4 photos of your hands so we can print your perfect manicure
               </div>
             </div>
-            <div className="col-12 col-md-2 icon-container">
+            <div className="col-12 col-md-3 icon-container">
               <img className="icon" src="https://d1raprm0ezfai1.cloudfront.net/Website+images+used/Photo+Fillers/nail.png" />
               <div className="icon-line"></div>
-              <div className="icon-text">
+              <div className="subscription-icon-text">
                 Receive your perfectly fitted manicures and refresh your nails in minutes</div>
               </div>
             <div className="col-12 text-align-center"><button className="detail-add-cart subscription-button">Try Pamper.Me</button></div>
